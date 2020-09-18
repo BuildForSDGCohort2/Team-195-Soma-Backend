2ae6f7101093801aa9e59812b30b3add78db8395
Mhady <amadousydney5@gmail.com>
2020-09-18 00:56:15 +0000

    new class to manage entries(add,update)

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
+
+    protected $fillable=['name','code'];
+
+    public function course()
+    {
+        return $this->hasMany("App\Course");
+    }
+
+    
 }
--- app/Lesson.php
+++ app/Lesson.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Lesson extends Model
 {
     //
+    protected $fillable=['lesson_number','name','writing_explanation'];
+
+    public function course()
+    {
+        return $this->belongsTo("App\Course");
+    }
 }
