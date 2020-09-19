96051928b252aba7679c617695640548433b3667
Mhady <amadousydney5@gmail.com>
2020-09-19 02:14:05 +0000

    Some changes

--- database/migrations/2020_09_19_004333_create_course_cat_table.php
+++ database/migrations/2020_09_19_004333_create_course_cat_table.php
@@ -13,15 +13,18 @@ class CreateCourseCatTable extends Migration
      */
     public function up()
     {
-        Schema::create('course_cat', function (Blueprint $table) {
+        Schema::create('course_cat_lang', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger("course_id");
             $table->unsignedBigInteger("category_id");
+            $table->unsignedBigInteger("language_id");
             $table->foreign("course_id","fk_course_category")->references("id")->on("course")
                                                             ->onDelete("cascade")->onUpdate("cascade");
             $table->foreign("category_id","fk_category_course")->references("id")->on("category")
                                                             ->onDelete("cascade")->onUpdate("cascade");
-            $table->unique(["id","course_id","category_id"],"course_cat_course_id_category_id_primary_key");
+            $table->foreign("language_id","fk_lang_course")->references("id")->on("language")
+                                                            ->onDelete("cascade")->onUpdate("cascade");
+            $table->unique(["id","course_id","category_id","language_id"],"id_course_id_category_id_lang_id_primary_key");
             $table->timestamps();
         });
     }
@@ -33,6 +36,6 @@ class CreateCourseCatTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('course_cat');
+        Schema::dropIfExists('course_cat_lang');
     }
 }
